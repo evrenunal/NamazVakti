57dabfa2b1e8510693e7ced767d68231d22b9424
evren unal <evren07unal@gmail.com>
2019-03-07 14:48:20 +0300

    Eexept settings It is ready for first test

--- NamazVakti/NamazVakti/Organizer.cs
+++ NamazVakti/NamazVakti/Organizer.cs
@@ -1,6 +1,7 @@
 ﻿using NamazVakti.Models;
 using NamazVakti.Services;
 using Newtonsoft.Json;
+using Plugin.LocalNotifications;
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 using System;
@@ -14,7 +15,7 @@ namespace NamazVakti
         private readonly StorageService strg;
         private readonly NamazApiService nmzApi;
         private static ISettings AppSettings;
-        private string defaultIlce = "9541";
+        private string defaultIlce = "9225";
 
         public Organizer()
         {
@@ -23,11 +24,12 @@ namespace NamazVakti
             nmzApi = new NamazApiService();
         }
 
-        //  CrossLocalNotifications.Current.Show("title", count++.ToString());
+       
         internal void RunCycle()
         {
-            DateTime currentDtTime = DateTime.Now;
-            var thisMonthId = currentDtTime.ToString("yy-MM");
+            var cyckleStartTm = DateTime.Now;
+
+            var thisMonthId = cyckleStartTm.ToString("yy-MM");
 
             var (success, file) = strg.GetFile(thisMonthId);
             DailyTimes[] dailyTimes = null;
@@ -45,12 +47,41 @@ namespace NamazVakti
                 var fileToSave = JsonConvert.SerializeObject(dailyTimes);
 
                 strg.SaveFile(fileToSave, thisMonthId);
-                var lastMonthId = currentDtTime.AddMonths(-1).ToString("yy-MM");
+                var lastMonthId = cyckleStartTm.AddMonths(-1).ToString("yy-MM");
                 strg.DeleteFile(lastMonthId);
             }
 
-            var todaysData = dailyTimes.FirstOrDefault(dd => dd.Date == currentDtTime.Date);
+            var todaysData = dailyTimes.FirstOrDefault(dd => dd.Date == cyckleStartTm.Date);
 
+            if (DateTime.Now.Date > cyckleStartTm) RunCycle();
+
+            switch (DateTime.Now.TimeOfDay)
+            {
+                case var now when todaysData.Imsak.TimeOfDay < now && now < todaysData.Gunes.TimeOfDay:
+                    AlertUser("Sabah", todaysData.Gunes.TimeOfDay - now);
+                    break;
+                case var now when todaysData.Ogle.TimeOfDay < now && now < todaysData.Ikindi.TimeOfDay:
+                    AlertUser("Öğle", todaysData.Gunes.TimeOfDay - now);
+                    break;
+                case var now when todaysData.Ikindi.TimeOfDay < now && now < todaysData.Aksam.TimeOfDay:
+                    AlertUser("İkindi", todaysData.Ikindi.TimeOfDay - now);
+                    break;
+                case var now when todaysData.Aksam.TimeOfDay < now && now < todaysData.Yatsi.TimeOfDay:
+                    AlertUser("Akşam", todaysData.Aksam.TimeOfDay - now);
+                    break;
+                case var now when todaysData.Yatsi.TimeOfDay < now && now < todaysData.Imsak.TimeOfDay:
+                    AlertUser("Yatsı", todaysData.Yatsi.TimeOfDay - now);
+                    break;
+            }
+
+        }
+
+        private void AlertUser(string prayKind, TimeSpan remainingTime)
+        {
+              CrossLocalNotifications.Current.Show("Namaz Vakti",
+                  $"{ prayKind} vaktine kalan süre:{Environment.NewLine}{remainingTime.Hours}:{remainingTime.Minutes}"+
+                  $"{Environment.NewLine}Bildirim Zamanı: {DateTime.Now.ToString("HH:mm")}"
+                  );
         }
     }
 }
